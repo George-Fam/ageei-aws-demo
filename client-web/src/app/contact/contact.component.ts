0cf9e3007198357c2962943221503e5dfd6cd1a3
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2022-01-01 19:00:30 -0500

    export execs for cleaner file

--- client-web/src/app/contact/contact.component.ts
+++ client-web/src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Exec } from './exec';
+import { execsConst } from './execs';
 
 @Component({
   selector: 'app-contact',
@@ -15,55 +16,6 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.execs = [
-      {
-        position: 'président',
-        fullname: 'Pierre-Olivier Brillant',
-        email: 'president@ageei.org',
-        picture: 'assets/contact/execs/president.jpg',
-        github: 'PierreOlivierBrillant',
-        linkedin: 'pierreolivier-brillant',
-      },
-      {
-        position: 'secrétaire',
-        fullname: 'Zachary Péloquin',
-        email: 'secretaire@ageei.org',
-        picture: 'assets/contact/execs/secretaire.jpg',
-        github: 'ZacharyPeloquin',
-        linkedin: 'zachary-peloquin-b3994b1ba',
-      },
-      {
-        position: 'trésorier',
-        fullname: 'Armand Brière',
-        email: 'tresorier@ageei.org',
-        picture: 'assets/contact/execs/tresorier.jpg',
-        github: 'ArmandBriere',
-        linkedin: 'armand-briere',
-      },
-      {
-        position: 'vp-interne',
-        fullname: 'Fanny Lavergne',
-        email: 'interne@ageei.org',
-        picture: 'assets/contact/execs/interne.jpg',
-        github: 'lesGrandsBras',
-        linkedin: 'fanny-lavergne-aa79442a',
-      },
-      {
-        position: 'vp-loisirs',
-        fullname: 'Simon Désormeaux',
-        email: 'loisirs@ageei.org',
-        picture: 'assets/contact/execs/loisirs.jpg',
-        github: 'sdxsofteng',
-        linkedin: 'simon-désormeaux-9326221b6',
-      },
-      {
-        position: 'vp-techno',
-        fullname: 'Lancelot Normand',
-        email: 'techno@ageei.org',
-        picture: 'assets/contact/execs/techo.jpg',
-        github: 'lancelotnd',
-        linkedin: 'lancelot-normand-456120122',
-      },
-    ];
+    this.execs = execsConst;
   }
 }
---
+++ client-web/src/app/contact/execs.ts
@@ -0,0 +1,52 @@
+import { Exec } from './exec';
+
+export const execsConst = [
+  {
+    position: 'président',
+    fullname: 'Pierre-Olivier Brillant',
+    email: 'president@ageei.org',
+    picture: 'assets/contact/execs/president.jpg',
+    github: 'PierreOlivierBrillant',
+    linkedin: 'pierreolivier-brillant',
+  },
+  {
+    position: 'secrétaire',
+    fullname: 'Zachary Péloquin',
+    email: 'secretaire@ageei.org',
+    picture: 'assets/contact/execs/secretaire.jpg',
+    github: 'ZacharyPeloquin',
+    linkedin: 'zachary-peloquin-b3994b1ba',
+  },
+  {
+    position: 'trésorier',
+    fullname: 'Armand Brière',
+    email: 'tresorier@ageei.org',
+    picture: 'assets/contact/execs/tresorier.jpg',
+    github: 'ArmandBriere',
+    linkedin: 'armand-briere',
+  },
+  {
+    position: 'vp-interne',
+    fullname: 'Fanny Lavergne',
+    email: 'interne@ageei.org',
+    picture: 'assets/contact/execs/interne.jpg',
+    github: 'lesGrandsBras',
+    linkedin: 'fanny-lavergne-aa79442a',
+  },
+  {
+    position: 'vp-loisirs',
+    fullname: 'Simon Désormeaux',
+    email: 'loisirs@ageei.org',
+    picture: 'assets/contact/execs/loisirs.jpg',
+    github: 'sdxsofteng',
+    linkedin: 'simon-désormeaux-9326221b6',
+  },
+  {
+    position: 'vp-techno',
+    fullname: 'Lancelot Normand',
+    email: 'techno@ageei.org',
+    picture: 'assets/contact/execs/techo.jpg',
+    github: 'lancelotnd',
+    linkedin: 'lancelot-normand-456120122',
+  },
+] as Exec[];
