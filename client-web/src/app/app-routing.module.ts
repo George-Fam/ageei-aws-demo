b4d904d349decdc7e15274bfb8a3345b1a7c664c
George Fam <george.fam@famcode.net>
2026-03-10 01:45:42 -0400

    chore: Migrate to Angular 16

--- client-web/src/app/app-routing.module.ts
+++ client-web/src/app/app-routing.module.ts
@@ -35,7 +35,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules,
-      relativeLinkResolution: 'legacy',
     }),
   ],
   exports: [RouterModule],
