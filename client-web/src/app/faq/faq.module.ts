022edcea76907e05babdf23eac51e422c3fcbe06
George Fam <george.fam@famcode.net>
2026-04-14 01:27:35 -0400

    feat(faq): Intégrer Directus CMS

--- client-web/src/app/faq/faq.module.ts
+++ client-web/src/app/faq/faq.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { FaqComponent } from './faq.component';
 import { SharedModule } from '../shared/shared.module';
 import { FaqRoutingModule } from './faq-routing.module';
-import { MarkdownModule } from 'ngx-markdown';
-import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [FaqComponent],
-  imports: [SharedModule, FaqRoutingModule, MarkdownModule.forRoot({ loader: HttpClient })],
+  imports: [SharedModule, FaqRoutingModule],
 })
 export class FaqModule {}
