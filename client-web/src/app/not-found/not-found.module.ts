58d86f5775378a66f3f46676e845538570674de9
George Fam <george.fam@famcode.net>
2026-05-04 13:50:18 -0400

    feat: add dedicated 404 page (#4)
    Replace silent redirect to home on unknown routes with a dedicated
    NotFoundModule that displays a "Page introuvable" message and a
    back-to-home button.

---
+++ client-web/src/app/not-found/not-found.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './not-found.component';
+import { NotFoundRoutingModule } from './not-found-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [NotFoundComponent],
+  imports: [SharedModule, NotFoundRoutingModule],
+})
+export class NotFoundModule {}
