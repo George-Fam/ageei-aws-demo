05e2e1ca4da1009d0022ffddfcdcecbc04b7f8be
George Fam <george.fam@famcode.net>
2026-06-11 14:07:42 -0400

    refactor: configure full GCal URLs to simplify timeline

--- client-web/src/app/timeline/timeline.component.ts
+++ client-web/src/app/timeline/timeline.component.ts
@@ -61,22 +61,10 @@ export class TimelineComponent implements OnInit {
     this.hasError = false;
     this.pendingFragmentEventId = this.normalizeEventFragment(this.route.snapshot.fragment);
 
-    if (environment.googleCalendarId && environment.googleCalendarId.length > 0) {
-      const base = 'https://calendar.google.com/calendar/embed';
-      const params = new URLSearchParams({
-        src: environment.googleCalendarId,
-        ctz: environment.googleCalendarTimeZone || 'UTC',
-      });
-
-      const url = `${base}?${params.toString()}`;
-      this.calendarUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    if (environment.googleCalendarUrl && environment.googleCalendarUrl.length > 0) {
+      this.calendarUrl = this.sanitizer.bypassSecurityTrustResourceUrl(environment.googleCalendarUrl);
       this.showCalendar = true;
-
-      if (environment.googleCalendarId) {
-        this.subscribeUrl = `https://calendar.google.com/calendar/u/0/r?cid=${encodeURIComponent(
-          environment.googleCalendarId
-        )}`;
-      }
+      this.subscribeUrl = environment.googleCalendarSubscribeUrl || undefined;
     }
     this.timelineService.getEvents().subscribe({
       next: (data) => {
