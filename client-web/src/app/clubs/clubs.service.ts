5ec3d05adb8beb5c5460b8aebb98c7736ff6ae99
George Fam <george.fam@famcode.net>
2026-08-20 18:47:14 -0400

    fix prettier check

--- client-web/src/app/clubs/clubs.service.ts
+++ client-web/src/app/clubs/clubs.service.ts
@@ -12,9 +12,7 @@ export class ClubsService {
 
   getClubs(): Observable<Club[]> {
     return this.http
-      .get<{ data: DirectusClub[] }>(
-        `${environment.clubsUrl}?sort=sort,name&limit=-1&filter[status][_eq]=published`,
-      )
+      .get<{ data: DirectusClub[] }>(`${environment.clubsUrl}?sort=sort,name&limit=-1&filter[status][_eq]=published`)
       .pipe(map(({ data }) => data.map((club) => this.toClub(club))));
   }
 
